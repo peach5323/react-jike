1c85ea1e2c1c579551acc2ba174fed6700a968be
peach <peach5323@hotmail.com>
2024-03-31 14:09:55 +0800

    echart基础demo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,36 @@
+import * as echarts from 'echarts'
+import { useEffect } from 'react';
+
 const Home = () => {
-  return <div>home</div>
+
+  
+  useEffect(() => {
+    // 保证dom可用 才进行图标的渲染
+    // 渲染图表的dom节点
+    const chartDom = document.getElementById('main');
+    // 图表初始化实例化对象
+    const myChart = echarts.init(chartDom);
+    // 图表参数
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    };
+    // 使用图标参数，完成渲染
+    option && myChart.setOption(option);
+})
+
+  return <div><div id="main" style={{width:'500px',height:'500px'}}></div></div>
 }
 
 export default Home
